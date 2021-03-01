26899b18ec84e126e263faf8d2aa75639f92e0e2
rsomasun <79770895+rsomasun@users.noreply.github.com>
2021-02-28 22:19:35 -0700

    Add name and edit reverseNumber()
    Adding name to line 2 
    Modified reverseNumber() to include the statement reverseNum = newNumber

--- GitDemo.java
+++ GitDemo.java
@@ -1,9 +1,10 @@
 /*****
- * Edit this line to have your name.
+ * Rohit Somasundaram
  ******/
 public class GitDemo {
     
     private double previousNumber;
+    private double reverseNum; 
     
     public GitDemo() {
         previousNumber = 1;
@@ -13,7 +14,7 @@ public class GitDemo {
      * This function should add 'num' to 'previousNumber' and return the sum.
     *****/
     public double addNumber(double num) {
-        System.out.print("Add num to previousNumber");
+        System.out.print(num + previousNumber);
         return 0;
     }
     
@@ -46,6 +47,7 @@ public class GitDemo {
             newNumber = newNumber * i;
         }
         
+        reverseNum = newNumber;
         return newNumber;
     }
     
